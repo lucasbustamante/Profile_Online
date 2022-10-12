09990b3f0f25763756842eaf9db7ba866ddee0c0
Lucas Bustamante <lucascostabustamante@gmail.com>
2022-10-11 23:29:10 -0300

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_site/View/photo.dart';
+import 'package:flutter_web_site/view/photo.dart';
 import 'package:flutter_web_site/colors/colors.dart';
 
 import 'model/liners.dart';
