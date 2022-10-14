44769ddeedf7245b3c965a3a0618aa8ee4d2785e
toshiossada <lucascostabustamante@gmail.com>
2022-10-13 23:42:09 -0300

    teste

--- lib/colors/colors.dart
+++ lib/colors/colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 const kBackground = Color(0xff18100A);
 const kUnderBackground = Color(0xff341704);
 const kUnderBackground2 = Color(0x6E341704);
-const kPrimaryColor = Color(0xfffe8a2c);
+const kPrimaryColor = Colors.red;
 const kUnderButton = Color(0xff364D2A);
 const kButton = Color(0xff3EDB41);
